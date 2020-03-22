f36594736e7e5d0a1d8a5bd662b833c0adc12a37
Murbagus <refi.bahar@gmail.com>
2020-03-22 23:10:37 +0700

    Tambah jumlah aktif

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 from login import getPage
 
 # Input url
-url = input('Masukan URL: ')
+url = input('Masukan URL (topik forum diskusi): ')
 username = input('Masukan username: ')
 password = input('Masukan password: ')
 
-# Ambil para kreator posting di forum
+# Ambil para author posting di forum
 print('')
 print('Mulai scraping...')
 page = getPage(url, username, password)
@@ -15,19 +15,28 @@ bsObj = BeautifulSoup(page, features='html.parser')
 print('Mengambil author...')
 print('')
 print('=============================================================')
-author_list = []
+author_discussion = []
 authors = bsObj.findAll('div', {'class': 'author'})
 for author in authors:
     x = author.find('a').get_text()
-    if x not in author_list:
-        author_list.append(x)
+    # if x not in author_discussion:
+    #     author_discussion.append(x)
+    author_discussion.append(x)
+
+# Group nama author menjadi satu (tidak ada yang lebih dari satu)
+author_list = []
+for author in author_discussion:
+    # Hitung jumlah aktif
+    aktif = author_discussion.count(author)
+    if not any(x.get('nama') == author for x in author_list):
+        author_list.append({'nama': author, 'jumlah_aktif': aktif})
 
 # Hapus author pertama karena merupakan dosen
 author_list.pop(0)
 # Print author ascending
-author_list.sort()
-for author in author_list:
-    print('>> ' + author)
+for author in sorted(author_list, key=lambda i: i['nama']):
+    print('>> ' + author['nama'] +
+          '\t\t (aktif: ' + str(author['jumlah_aktif']) + ')')
 
 print('=============================================================')
 print('')
